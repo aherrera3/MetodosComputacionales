97029f433ace48fda38ee8cf2150e6da25bca9d7
aherrera3 <52289646+aherrera3@users.noreply.github.com>
2019-06-28 21:26:52 -0500

    Add files via upload

---
+++ HerreraAngelicaS4C4/primos.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a, b, start, stop, contador;
+    
+    cout<<"Introduce el primer número";
+    cin>>a;
+    
+    cout<<"Introduce el segundo número";
+    cin>>b;
+    
+    if(a<b)
+    {
+        start = a;
+        stop = b;
+    }
+    
+    else
+    {
+        start = b;
+        stop = a;
+    }
+    
+    for(int i=start ; i<=stop ; i++)
+    {
+        contador = 0;
+        
+        for(int j=2 ; j<i ; j++)
+        {
+            if(i%j==0)
+            {
+                contador = contador +1;
+            }   
+        }
+        
+        if(contador == 0)
+        {
+            int primo = i;
+            cout << primo << endl;
+        }
+    }   
+    
+    return 0;
+}
