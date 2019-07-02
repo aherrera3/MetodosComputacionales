3c99262b9017e10f0ab3b674e6a54470d05ced26
aherrera3 <52289646+aherrera3@users.noreply.github.com>
2019-07-02 11:41:36 -0500

    terminado 1

--- HerreraAngelicaS5C1/producto_array_FUN.cpp
+++ HerreraAngelicaS5C1/producto_array_FUN.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+using namespace std;
+
+//Declaracion de funciones
+int * producto(int arr1[5], int arr2[5]);
+int producto_punto(int arr1[5], int arr2[5]);
+
+int *apuntador;
+int arr[5];
+
+int arr1[5] = {1,2,3,4,5};
+int arr2[5] = {10,20,30,40,50};
+
+int prod;
+
+int main ()
+{   
+    producto(arr1,arr2);
+    //cout << "El producto entre arr1 por arr2 es: "  << producto(arr1,arr2) << endl;
+    cout << "El producto  punto entre arr1 por arr2 es: " << producto_punto(arr1,arr2) << endl;
+           
+    return 0;
+}
+
+int * producto (int arr1[5], int arr2[5])
+{       
+    for(int i = 0; i<5 ; i++)
+    {
+        arr[i] = arr1[i] * arr2[i]; 
+    }
+    
+    apuntador = arr;
+    
+    cout << "El producto entre arr1 por arr2 es: " << *apuntador << endl;
+}
+
+int producto_punto(int arr1[5], int arr2[5])
+{       
+    prod = 0;
+    
+    for(int i = 0; i<5 ; i++)
+    {
+        prod = prod + arr1[i] * arr2[i];   
+    }
+    
+    return prod;
+}
+
 
