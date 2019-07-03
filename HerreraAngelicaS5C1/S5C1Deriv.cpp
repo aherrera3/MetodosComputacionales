29a761cc963c5005b45f92ba97d55d2d424ebd0e
Default user <angelicaherreraalba@gmail.com>
2019-07-03 02:35:48 +0000

    acance de derivada

--- HerreraAngelicaS5C1/S5C1Deriv.cpp
+++ HerreraAngelicaS5C1/S5C1Deriv.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 //Declaracion de variables
 int *apuntador;  //declaracion del apuntador
 
 //Declaracion de mi funcion
-int derivada();
+int derivada(double);
 
 int main()
 {
@@ -21,7 +22,7 @@ int main()
     cout << "Introduce el número de puntos del intervalo: "; //cuento como puntos a mis dos extremos
     cin >> n;
     
-    double x[n];
+    double x[n], coseno[n];
     
     if(a<b)
     {
@@ -37,21 +38,36 @@ int main()
     
     dx = (stop-start)/(n-1);
     x[0] = start;
-    cout << "mi arreglo x es: [" << x[0] << ", ";
+    coseno[0] = cos(x[0]);   //toma a los x como radianes
+    
+    //cout << "mi arreglo x es: [" << x[0] << ", ";
+    cout << "mi coseno(x) es: [" << coseno[0] << ", ";    
    
     for(int i = 1; i<n ; i++)
     {
         x[i] = x[i-1] + dx;
+        coseno[i] = cos(x[i]);
         
-        if(i == n-1) cout << x[i] << "]" << endl;
-        
-        else cout << x[i] << ", ";
-    }    
+        if(i == n-1) {
+           // cout << x[i] << "]" << endl;
+            cout << coseno[i] << "]" << endl;
+        }        
+        else {
+           // cout << x[i] << ", ";
+            cout << coseno[i] << ", ";
+        }
+    }        
     
     return 0;
 }
 
-int derivada()
+int derivada(double f)
 {
-    return 0;   
+    for(int i =1 ; i<n ; i++)
+    {
+        (f[i]-f[0])/
+    }
+    
+    
+    return der;   
 }
