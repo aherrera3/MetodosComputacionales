71cbe85190fd35c985a394a5f0ad987df45e847d
Default user <angelicaherreraalba@gmail.com>
2019-07-03 22:30:13 +0000

    plots terminado

---
+++ HerreraAngelicaS5C1/plotsS5C1.py
@@ -0,0 +1,19 @@
+#script para realizar grafica de coseno y su derivada
+import numpy as np
+import matplotlib.pyplot as plt
+
+datos = np.genfromtxt("datos.dat")
+
+x = datos[:,0]
+cos = datos[:,1]
+sen = datos[:,2]
+
+plt.figure()
+plt.plot(x, cos, label="coseno")
+plt.plot(x, sen, label="derivada numerica de coseno")
+plt.xlabel("x (radianes)")
+plt.ylabel("f (x)")
+plt.legend()
+plt.title("Coseno con su derivada numerica")
+plt.grid()
+plt.savefig("S5C1PLOT")
