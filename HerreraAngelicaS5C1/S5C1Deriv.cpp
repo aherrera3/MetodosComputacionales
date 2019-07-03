d01b80d7a0427227086935f44c3e4b8810b28c0f
Default user <angelicaherreraalba@gmail.com>
2019-07-03 22:18:07 +0000

    derivada imprimiendo coseno

--- HerreraAngelicaS5C1/S5C1Deriv.cpp
+++ HerreraAngelicaS5C1/S5C1Deriv.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 using namespace std;
 
-//Declaracion de variables
-int *apuntador;  //declaracion del apuntador
-double a, b, start, stop, dx;
+//variables globales
+double *p_x;             //apuntador a mi arreglo x
+double *p_coseno;        //apuntador a mi arreglo coseno
+double *p_derivada;      //aputador a mi arreglo de derivadas 
+double a, b, start, stop, dx, x[1000], coseno[1000], der[1000];
 int n;
 
-//Declaracion de mi funcion
-int derivada(double);
+//declaro mi funcion derivada
+double * derivada(double*, double*);
 
+//metodo constructor
 int main()
 {   
+    //abro y creo un archivo llamado datos.dat
+    ofstream archivo;
+    archivo.open("datos.dat");
+    
     cout<<"Introduce el primer número entero: ";
     cin>>a;
     
@@ -20,8 +28,6 @@ int main()
     
     cout << "Introduce el número de puntos del intervalo: "; //cuento como puntos a mis dos extremos
     cin >> n;
-    
-    double x[n], coseno[n];
     
     if(a<b)
     {
@@ -38,6 +44,7 @@ int main()
     dx = (stop-start)/(n-1);
     x[0] = start;
     coseno[0] = cos(x[0]);   //toma a los x como radianes
+      
     
     //cout << "mi arreglo x es: [" << x[0] << ", ";
     cout << "mi coseno(x) es: [" << coseno[0] << ", ";    
@@ -55,18 +62,33 @@ int main()
            // cout << x[i] << ", ";
             cout << coseno[i] << ", ";
         }
-    }        
+    }  
+    
+    //inicializo mis pointers de los arreglos x y coseno
+    p_x = x;
+    p_coseno = coseno;
+    
+    for(int i = 0; i<n-1 ;i++)
+    {
+        cout << derivada(p_x, p_coseno)[i] << ", ";
+        archivo << x[i] << " " << coseno[i] << " " << derivada(p_x, p_coseno)[i] << endl;  
+    }
+    
+    archivo.close();
     
     return 0;
 }
 
-int * derivada(double f)
+
+/*Funcion que calcula la derivada de coseno
+Return: pointer en donde se contiene la informacion de la derivada de coseno
+Parametros: apuntadores de coseno y de x
+*/
+
+double * derivada(double *p_x, double *p_coseno)
 {
-    for(int i = 2 ; i<=n ; i++)
-    {
-        der[i] = (f[i]-f[i-2])/(2*dx)
-    }
-    
-    
-    return der;   
-}
+    for(int i = 0 ; i<n ; i++) der[i] = (p_coseno[i+1]-p_coseno[i])/(dx);    //derivo por forward a todos mis elementos
+         
+    p_derivada = der;    
+    return p_derivada;   
+} 
