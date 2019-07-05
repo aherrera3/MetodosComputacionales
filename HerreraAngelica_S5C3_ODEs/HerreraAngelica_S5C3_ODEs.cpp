23c429d521c4baa1b2bf2cd0965fa3f977d0df0a
Default user <angelicaherreraalba@gmail.com>
2019-07-05 19:16:46 +0000

    .cpp avance

--- HerreraAngelica_S5C3_ODEs/HerreraAngelica_S5C3_ODEs.cpp
+++ HerreraAngelica_S5C3_ODEs/HerreraAngelica_S5C3_ODEs.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 //variables globales
-double m, k, x_1[100], x_2[100], t[100], x[100];
+double m, k, t[1000], x[1000], v[1000], dt;
+int n;
+
 double derivada(double, double);
 double segunda_derivada(double, double);
-void runge_kutta(double, double);
+double runge_kutta(double, double);
 
 int main()
 {
@@ -17,61 +19,69 @@ int main()
     
     n = 100;
     t[0] = 0;
-    t[n] = 5;
+    t[n-1] = 5;
     
-    dt = (t_final-t_inicial)/n
+    dt = (t[n-1]-t[0])/(n-1);
     
     //c.i
-    t[0] = 0;   //posicion en t=0
-    v[0] = 0;  
+    t[0] = 0;   //posicion en t=0 
     x[0] = 0.1;
-    x_1[0] = 0;
-    x_2[0] = 0;
+    v[0] = 0;
+    
+    cout << t[0] << endl;
+    
+    for(int i = 1; i<n; i++)
+    {
+        t[i] = t[i-1] + dt;
+        x[i] = runge_kutta(x[i-1], v[i-1]);
+        cout << x[i] << endl;
+    }
     
     return 0;
 }
 
-double derivada_x1(double x_1, double x_2)
+double derivada(double v)
 {
-    return x_2;
+    return v;
 }
 
-double derivada_x2(double x_1, double x_2)
+double derivada_segunda(double x)
 {
-    return -k/m * x_1;
+    return -k/m * x;
 }
 
 
-void runge_kutta(double x1_antes, double x2_antes)
+double runge_kutta(double x_antes, double v_antes)
 {
-    k1_der_1 = derivada_x1(x_antes);
-    k1_der_2 = derivada_x2(x_antes);
+        double k1_der_1 = derivada(v_antes);
+        double k1_der_2 = derivada_segunda(x_antes);
     
-    x11 = x1_antes + (dt/2) * k1_der_1;
-    x21 = x2_antes + (dt/2) * k1_der_2;
+        double x1 = x_antes + (dt/2) * k1_der_1;
+        double v1 = v_antes + (dt/2) * k1_der_2;
     
-    k2_der_1 = derivada_x1(x11, x21);
-    k2_der_2 = derivada_x2(x11, x21);
+        double k2_der_1 = derivada(v1);
+        double k2_der_2 = derivada_segunda(x1);
     
-    x12 =  x1_antes + (dt/2) * k2_der_1;
-    x22 = x2_antes + (dt/2) * k2_der_2;
+        double x2 =  x_antes + (dt/2) * k2_der_1;
+        double v2 = v_antes + (dt/2) * k2_der_2;
     
-    k3_der_1 = derivada_x1(x12);
-    k3_der_2 = derivada_x2(x22);
+        double k3_der_1 = derivada(v2);
+        double k3_der_2 = derivada_segunda(x2);
     
-    x13 =  x1_antes + dt * k3_der_1;  
-    x23  = x2_antes + dt * k3_der_2;
+        double x3 =  x_antes + dt * k3_der_1;
+        double v3  = v_antes + dt * k3_der_2;
         
-    k4_der_1 = derivada_x1(x13);
-    k4_der_2 = derivada_x2(x23);  
+        double k4_der_1 = derivada(v3);
+        double k4_der_2 = derivada_segunda(x3);
     
     
-    k1_promedio = (k1_der_1 + 2*k2_der_1 + 2*k3_der_1 + k4_der_1 )/6 ;
-    k2_promedio = (k1_der_2 + 2*k2_der_2 + 2*k3_der_2 + k4_der_2 )/6 ;  
+        double k1_promedio = (k1_der_1 + 2*k2_der_1 + 2*k3_der_1 + k4_der_1 )/6 ;
+        double k2_promedio = (k1_der_2 + 2*k2_der_2 + 2*k3_der_2 + k4_der_2 )/6 ;  
+        
+        double x_ahora = x_antes + dt * k1_promedio;
+        double v_ahora = v_antes + dt * k2_promedio;
     
-    t_ahora = t_antes + dt;
-    x1_ahora = x1_antes + h * k1_promedio;
-    x2_ahora = x2_antes + h * k2_promedio;
+    return x_ahora;
     
 }  
 /*  
