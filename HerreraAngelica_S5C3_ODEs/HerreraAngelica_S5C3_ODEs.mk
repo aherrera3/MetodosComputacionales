039ed87590e22bea732d7a8d7d024043ef61c076
aherrera3 <52289646+aherrera3@users.noreply.github.com>
2019-07-07 21:28:16 -0500

    Delete HerreraAngelica_S5C3_ODEs.mk

--- HerreraAngelica_S5C3_ODEs/HerreraAngelica_S5C3_ODEs.mk
+++
@@ -1,9 +0,0 @@
-all : HerreraAngelica_S5C3_ODEs.py
-	python HerreraAngelica_S5C3_ODEs.py
-
-HerreraAngelica_S5C3_ODEs.py : datos.dat
-	python HerreraAngelica_S5C3_ODEs.py
-
-datos.dat : HerreraAngelica_S5C3_ODEs.cpp
-	g++ HerreraAngelica_S5C3_ODEs.cpp
-	./a.out HerreraAngelica_S5C3_ODEs.cpp
