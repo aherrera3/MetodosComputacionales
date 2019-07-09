1aa3ecdb52a832ef9aa288941418765bd982e20e
Default user <angelicaherreraalba@gmail.com>
2019-07-09 02:59:10 +0000

    avance .cpp

--- HerreraAngelicaS6C1/HerreraAngelicaS6C1.cpp
+++ HerreraAngelicaS6C1/HerreraAngelicaS6C1.cpp
@@ -16,31 +16,35 @@ int main()
     dx = 0.01;
     
     //dx = (x[n]-x[0])/(n-1);
-    n = (2.0-0.0)/(dx) +1;
+    n = (2.0-0.0)/(dx);
+    cout << "mi n es = " << n << endl;
     
     x[0] = 0.0;
     x[n] = 2.0;
     
-    dt = dx*0.25/v;
-    r = v*dt/dx;     //debe ser menor que 1
+    dt = dx*0.25/v;  // r=0.25
+    r = v*dt/dx;     //r debe ser menor que 1
     
     cout << "mi r es = " << r << endl;
     
     u_inicial[0] = 1.0;
+    u_inicial[n] = 1.0;
+    
+    u_futuro[0] = 1.0;
+    u_futuro[0] = 1.0;
     
     cout << x[0] << "   " << u_inicial[0] << endl;
     
     //construyo mi funcion pulso rectangular en t=0
-    for(int i=1; i<n ;i++)
+    for(int i=1; i<=n ;i++)
     {
         x[i] = x[i-1] + dx;
-        if(x[i]>=0.75 && x[i]<=1.25) u_inicial[i] =  2.0; 
+        if(x[i]>=0.75 && x[i]<=1.25) u_inicial[i] = 2.0; 
         else u_inicial[i] = 1.0;
         
         cout << x[i] << "   " << u_inicial[i] << endl; 
     }
     
-    u_futuro[0] = 1.0;
     u_presente[0] = u_futuro[0];
     
     //hago mi primer futuro para obtener mi primer presente
@@ -52,7 +56,7 @@ int main()
     
     //para todos los demas futuros
     int j=0;
-    int n_tiempo = 100;
+    int n_tiempo = 1;
     
     archivo << x[0] << " " << u_inicial[0] << " " << u_presente[0] << endl;
     while(j<=n_tiempo)
@@ -60,8 +64,11 @@ int main()
         for(int i=1; i<n; i++)
         {
             u_futuro[i] = r*(u_presente[i] - u_presente[i-1]) + u_presente[i];
+        }
+        //mi futuro es ahora mi presente
+        for(int i=1; i<n; i++)
+        {
             u_presente[i] = u_futuro[i];
-            
             if(j==n_tiempo) archivo << x[i] << " " << u_inicial[i] << " " << u_presente[i] << endl;
         }
         j++;
