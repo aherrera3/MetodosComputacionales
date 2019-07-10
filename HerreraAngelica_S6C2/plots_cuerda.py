39a8c944d71eff632816d3fa3d4c4af1ceb0f7ea
Default user <angelicaherreraalba@gmail.com>
2019-07-10 16:23:58 +0000

    .py caso3 avance

--- HerreraAngelica_S6C2/plots_cuerda.py
+++ HerreraAngelica_S6C2/plots_cuerda.py
@@ -45,14 +45,14 @@ plt.savefig("extremos_fijos")
 
 #PARA UN EXTREMO LIBRE
 
-datos = np.genfromtxt("datos1.dat")
+datos1 = np.genfromtxt("datos1.dat")
 
-x = datos[:,0]
-u_t0_ = datos[:,1]
-u_t1_ = datos[:,2]
-u_t2_ = datos[:,3]
-u_t3_ = datos[:,4]
-u_t4_ = datos[:,5]
+x = datos1[:,0]
+u_t0_ = datos1[:,1]
+u_t1_ = datos1[:,2]
+u_t2_ = datos1[:,3]
+u_t3_ = datos1[:,4]
+u_t4_ = datos1[:,5]
 
 plt.figure(figsize=(10,7))
 plt.subplot(2,2,1)
@@ -85,4 +85,50 @@ plt.grid()
 
 plt.suptitle("Cuerda con extremos libres en diferentes tiempos")
 plt.savefig("extremos_libres")
+
+
+#PARA UN EXTREMO MODULADO POR F EXTER
+datos2 = np.genfromtxt("datos2.dat")
+
+x = datos2[:,0]
+_u_t0_ = datos2[:,1]
+_u_t1_ = datos2[:,2]
+_u_t2_ = datos2[:,3]
+_u_t3_ = datos2[:,4]
+_u_t4_ = datos2[:,5]
+
+plt.figure(figsize=(10,7))
+plt.subplot(2,2,1)
+plt.plot(x,_u_t0_)
+plt.plot(x,_u_t1_)
+plt.title("t1")
+plt.ylabel("amplitud")
+plt.grid()
+
+plt.subplot(2,2,2)
+plt.plot(x,_u_t0_)
+plt.plot(x,_u_t2_)
+plt.title("t2")
+plt.grid()
+
+plt.subplot(2,2,3)
+plt.plot(x,_u_t0_)
+plt.plot(x,_u_t3_)
+plt.title("t3")
+plt.ylabel("amplitud")
+plt.xlabel("x")
+plt.grid()
+
+plt.subplot(2,2,4)
+plt.plot(x,_u_t0_)
+plt.plot(x,_u_t4_)
+plt.title("t4")
+plt.xlabel("x")
+plt.grid()
+
+plt.suptitle("Cuerda con extremo modulado por F_ext en diferentes tiempos")
+plt.savefig("extremo_modulado")
+
+
+
 
