21a43d242268c15577702637093253d4f66102e6
Default user <angelicaherreraalba@gmail.com>
2019-07-12 16:26:36 +0000

    extre libre terminado y graficas

--- HerreraAngelica_S6C4/difusion.cpp
+++ HerreraAngelica_S6C4/difusion.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
     double l, nu, dx, dt, r, x[100], y[100];
-    double t[10000], T[100][100], T_presente[100][100], T_futuro[100][100];
+    double t[5000], T[100][100], T_presente[100][100], T_futuro[100][100], T_mean[10000];
+    double T_pre[100][100], T_fut[100][100], T_mean_[10000];
     int n;
     
     l = 1.0;
@@ -28,51 +29,47 @@ int main()
         y[i] = y[i-1] + dx;
     }
     
-    ofstream matriz;
-    matriz.open("matriz.dat");
+    ofstream t_inicial;
+    t_inicial.open("t_inicial.dat");
     
     ofstream datos;
     datos.open("datos.dat");
     
-    //llenos los valores de temperaturas de la placa
+    //llenos los valores de temperaturas de la placa y cond de frontera fijas a T=50 para mi primer futuro
     for(int i=0; i<n; i++)
     {
+        //cond de fronteras fijas
+        T_futuro[i][0] = 50.0;
+        T_futuro[i][n-1] = 50.0;
+        
         for(int j=0; j<n; j++)
         {
+            //cond de fronteras fijas
+            T_futuro[0][j] = 50.0;
+            T_futuro[n-1][j] =50.0;
             if(y[i]>=l/2.0-0.2 && y[i]<=l/2.0+ 0.2 && x[j]>=0.2 && x[j]<=0.4)   //si esta dentro de mis valores de x y y correctos
             {
                 T[i][j] = 100.0;
                 T_presente[i][j] =  T[i][j];
+                T_pre[i][j] = T[i][j];
             }
             
             else 
             {
                 T[i][j] = 50.0;
                 T_presente[i][j] =  T[i][j];
+                T_pre[i][j] = T[i][j];
             }
             
-            matriz << T[i][j] << " ";
+            t_inicial << T[i][j] << " ";
         }
-        matriz << endl;
+        t_inicial << endl;
         datos << x[i] << " " << y[i] << endl;
     }
     
     datos.close();
-    matriz.close();
-
-    //cond de frontera fijas a T=50
-    for(int i=0; i<n; i++)
-    {
-        T_futuro[i][0] = 50.0;
-        T_futuro[i][n-1] = 50.0;
-        
-        for(int j=0; j<n; j++)
-        {
-            T_futuro[0][j] = 50.0;
-            T_futuro[n-1][j] =50.0;
-        }
-    }
-    
+    t_inicial.close();
+   
     
     //archivos para pasar mi matriz T en diferentes tiempos
     ofstream archivo_t1;
@@ -81,38 +78,73 @@ int main()
     ofstream archivo_t2;
     archivo_t2.open("t2.dat");
     
+    ofstream archivo_t1_;
+    archivo_t1_.open("t1_libres.dat");
     
-    for(int k=1; k<=10000; k++) //for del tiempo
+    ofstream archivo_t2_;
+    archivo_t2_.open("t2_libres.dat");
+    
+    
+    for(int k=1; k<=5000; k++) //for del tiempo
     {
         //t[k] = t[k-1] + dt;
         
-        //hago mi nuevo futuro
-        for(int i=1; i<n; i++)
+        //cond de frontera libres
+        for(int i=0; i<n; i++)
         {
-            for(int j=1; j<n ; j++)
+            T_fut[i][0] = T_fut[i][1];
+            T_fut[i][n-1] = T_fut[i][n-2];
+            
+            for(int j=0; j<n ; j++)
             {
-                T_futuro[i][j] = r*(T_presente[i+1][j] - 2*T_presente[i][j] + T_presente[i-1][j] + T_presente[i][j+1] - 2*T_presente[i][j] + T_presente[i][j-1]) + T_presente[i][j];
+                T_fut[0][j] = T_fut[1][j];
+                T_fut[n-1][j] = T_fut[n-2][j];   //bien
+                
+                if(i!=0 && j!=0 && i!=n-1 && j!=n-1)
+                {
+                    T_futuro[i][j] = r*(T_presente[i+1][j] - 2*T_presente[i][j] + T_presente[i-1][j] + T_presente[i][j+1] - 2*T_presente[i][j] + T_presente[i][j-1]) + T_presente[i][j];
+                    T_fut[i][j] = r*(T_pre[i+1][j] - 2*T_pre[i][j] + T_pre[i-1][j] + T_pre[i][j+1] - 2*T_pre[i][j] + T_pre[i][j-1]) + T_pre[i][j];
+                }
             }
         }
     
-        //actualizo mi presente
+        //actualizo mis presentes
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<n ; j++)
             {
                 T_presente[i][j] = T_futuro[i][j];
+                T_pre[i][j] = T_fut[i][j];
             
-                if(k==100) archivo_t1 << T_presente[i][j] << " ";
-                if(k==2500) archivo_t2 << T_presente[i][j] << " ";
+                if(k==100)
+                {
+                    archivo_t1 << T_presente[i][j] << " ";
+                    archivo_t1_ << T_pre[i][j] << " ";
+                }
+                if(k==2500) 
+                {
+                    archivo_t2 << T_presente[i][j] << " ";
+                    archivo_t2_ << T_pre[i][j] << " ";
+                }
             }
             
-            if(k==100) archivo_t1 << endl;
-            if(k==2500) archivo_t2 << endl;
+            if(k==100) 
+            {
+                archivo_t1 << endl;
+                archivo_t1_ << endl;
+            }
+            if(k==2500) 
+            {
+                archivo_t2 << endl;
+                archivo_t2_ << endl;
+            }
         }
     }
     
     archivo_t1.close();
     archivo_t2.close();
+    archivo_t1_.close();
+    archivo_t2_.close();
     
     return 0;
 }
--- HerreraAngelica_S6C4/plots_Difusion.py
+++ HerreraAngelica_S6C4/plots_Difusion.py
@@ -6,12 +6,11 @@ datos = np.genfromtxt("datos.dat")
 x = datos[:,0]
 y = datos[:,1]
 
-matriz = np.genfromtxt("matriz.dat")
+x,y = np.meshgrid(x, y)
+
+matriz = np.genfromtxt("t_inicial.dat")
 t1 = np.genfromtxt("t1.dat")
 t2 = np.genfromtxt("t2.dat")
-
-
-x,y = np.meshgrid(x, y)
 
 plt.figure()
 ax = plt.axes(projection='3d')
@@ -24,20 +23,72 @@ plt.savefig("T_inicial")
 
 
 plt.figure()
-ax1 = plt.axes(projection='3d')
-ax1.plot_surface(x, y, t1)
-ax1.set_title('T(x,y,t) en t=100s')
+ax = plt.axes(projection='3d')
+ax.plot_surface(x, y, t1)
+ax.set_title('T(x,y,t) en t=100s')
 plt.xlabel("x")
 plt.ylabel("y")
-ax1.set_zlabel("z")
+ax.set_zlabel("z")
 plt.savefig("T_100s")
 
 
 plt.figure()
-ax2 = plt.axes(projection='3d')
-ax2.plot_surface(x, y, t2)
-ax2.set_title('T(x,y,t) en t=2500s')
+ax = plt.axes(projection='3d')
+ax.plot_surface(x, y, t2)
+ax.set_title('T(x,y,t) en t=2500s')
 plt.xlabel("x")
 plt.ylabel("y")
-ax2.set_zlabel("z")
+ax.set_zlabel("z")
 plt.savefig("T_2500s")
+
+
+#extremos libres
+
+t1_ = np.genfromtxt("t1_libres.dat")
+t2_ = np.genfromtxt("t2_libres.dat")
+
+plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(x, y, t1_)
+ax.set_title('T(x,y,t) en t=100s ext libres')
+plt.xlabel("x")
+plt.ylabel("y")
+ax.set_zlabel("z")
+plt.savefig("T_100s_libres")
+
+
+plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(x, y, t2_)
+ax.set_title('T(x,y,t) en t=2500s ext libres')
+plt.xlabel("x")
+plt.ylabel("y")
+ax.set_zlabel("z")
+plt.savefig("T_2500s_libres")
+
+"""
+#extremos periodicos
+
+_t1_ = np.genfromtxt("t1_libres.dat")
+_t2_ = np.genfromtxt("t2_libres.dat")
+
+plt.figure()
+ax4 = plt.axes(projection='3d')
+ax4.plot_surface(x, y, _t1_)
+ax4.set_title('T(x,y,t) en t=100s ext libres')
+plt.xlabel("x")
+plt.ylabel("y")
+ax4.set_zlabel("z")
+plt.savefig("T_100s_libres")
+
+
+plt.figure()
+ax5 = plt.axes(projection='3d')
+ax5.plot_surface(x, y, _t2_)
+ax5.set_title('T(x,y,t) en t=2500s ext libres')
+plt.xlabel("x")
+plt.ylabel("y")
+ax5.set_zlabel("z")
+plt.savefig("T_2500s_libres")
+"""
+
