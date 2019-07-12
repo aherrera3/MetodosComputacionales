be63da410f4245a4c7d9b57a3c1b2f2c1515e84a
Default user <angelicaherreraalba@gmail.com>
2019-07-12 17:09:51 +0000

    avance

--- HerreraAngelica_S6C4/plots_Difusion.py
+++ HerreraAngelica_S6C4/plots_Difusion.py
@@ -44,12 +44,12 @@ plt.savefig("T_2500s")
 
 #extremos libres
 
-t1_ = np.genfromtxt("t1_libres.dat")
-t2_ = np.genfromtxt("t2_libres.dat")
+t1_libres = np.genfromtxt("t1_libres.dat")
+t2_libres = np.genfromtxt("t2_libres.dat")
 
 plt.figure()
 ax = plt.axes(projection='3d')
-ax.plot_surface(x, y, t1_)
+ax.plot_surface(x, y, t1_libres)
 ax.set_title('T(x,y,t) en t=100s ext libres')
 plt.xlabel("x")
 plt.ylabel("y")
@@ -59,36 +59,35 @@ plt.savefig("T_100s_libres")
 
 plt.figure()
 ax = plt.axes(projection='3d')
-ax.plot_surface(x, y, t2_)
+ax.plot_surface(x, y, t2_libres)
 ax.set_title('T(x,y,t) en t=2500s ext libres')
 plt.xlabel("x")
 plt.ylabel("y")
 ax.set_zlabel("z")
 plt.savefig("T_2500s_libres")
 
-"""
+
 #extremos periodicos
 
-_t1_ = np.genfromtxt("t1_libres.dat")
-_t2_ = np.genfromtxt("t2_libres.dat")
+tiempo1 = np.genfromtxt("t1_periodicas.dat")
+tiempo2 = np.genfromtxt("t2_periodicas.dat")
 
 plt.figure()
 ax4 = plt.axes(projection='3d')
-ax4.plot_surface(x, y, _t1_)
-ax4.set_title('T(x,y,t) en t=100s ext libres')
+ax4.plot_surface(x, y, tiempo1)
+ax4.set_title('T(x,y,t) en t=100s ext periodicos')
 plt.xlabel("x")
 plt.ylabel("y")
 ax4.set_zlabel("z")
-plt.savefig("T_100s_libres")
+plt.savefig("T_100s_periodicos")
 
 
 plt.figure()
 ax5 = plt.axes(projection='3d')
-ax5.plot_surface(x, y, _t2_)
-ax5.set_title('T(x,y,t) en t=2500s ext libres')
+ax5.plot_surface(x, y, tiempo2)
+ax5.set_title('T(x,y,t) en t=2500s ext periodicos')
 plt.xlabel("x")
 plt.ylabel("y")
 ax5.set_zlabel("z")
-plt.savefig("T_2500s_libres")
-"""
+plt.savefig("T_2500s_periodicos")
 
