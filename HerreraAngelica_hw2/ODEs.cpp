377e86b32079b5c6288910d5d7ef5b4a5dad840f
Default user <angelicaherreraalba@gmail.com>
2019-07-15 22:28:49 +0000

    metodos terminados

--- HerreraAngelica_hw2/ODEs.cpp
+++ HerreraAngelica_hw2/ODEs.cpp
@@ -12,32 +12,26 @@ double  _r_[1000], _x_[1000], _y_[1000], _vx_[1000], _vy_[1000];
 int n;
 
 //Declaracion de las funciones
+double euler(double, double);
 double leap_frog(double, double);
-double leap_frog(double, double);
-double runge_kutta(double, double); 
+double runge_kutta(); 
+double aceleracion(double, double);
 
 int main()
 {
-    /*
-    ifstream infile("EarthOrbit.dat");
+    //creo archivos 
     
-    int i = 0;
-    while(!infile.eof())   //si no ha llegado al final del archivo
-    {
-        infile >> t[i] >> x[i] >> y[i] >> z[i];    // tomo los 4 valores de cada fila
-
-        // do something with them
-        r[i] = sqrt(pow(x[i],2) + pow(y[i],2) + pow(z[i],2));
-        i++;
-    }
-    */
+    ofstream archivo;
+    archivo.open("euler.dat");
+    ofstream archivo1;
+    archivo1.open("leap_frog.dat");
     
-    G = 6.674 * pow(10,-11);   //constante gravitacional  [N m^2/kg^2]
-    m = 1.989 * pow(10,30); 
+    G = 1.98256*pow(10,-29);    //6.674*pow(10,-11);   //constante gravitacional  [N m^2/kg^2]
+    m = 1.989*pow(10,30); 
     
     n = 1000;
     t[0] = 0.0;
-    t[n-1] = 20.0;   //20 orbitas = 20 años
+    t[n-1] = 1.0;   //20 orbitas = 20 años
         
     dt = (t[n-1]-t[0])/(n-1);
     cout << "mi dt es: " << dt;  
@@ -59,18 +53,50 @@ int main()
     _vy_[0] = vy[0];
     
     r[0] = sqrt(pow(x[0],2) + pow(y[0],2));          //para euler
-    r_[0] = sqrt(pow(x[0],2) + pow(y[0],2));         //para leap-frog  
-    _r_[0] = sqrt(pow(x[0],2) + pow(y[0],2));        //para runge-kutta
+    r_[0] = r[0];        //para leap-frog  
+    _r_[0] = r[0];       //para runge-kutta
     
+    archivo << x[0] << " " << y[0] << endl;
+    archivo1 << x_[0] << " " << y_[0] << endl;
     
     for(int i=1; i<n; i++)
     {
         //euler
-        v[i] = v[i-1] + dt*aceleracion(x[i-1], r[i-1]);
-        x[i] = euler(x[i-1], v[i-1]);    
+        vx[i] = vx[i-1] + dt*aceleracion(x[i-1], r[i-1]);
+        vy[i] = vy[i-1] + dt*aceleracion(y[i-1], r[i-1]);
+        x[i] = euler(x[i-1], vx[i-1]);
+        y[i] = euler(y[i-1], vy[i-1]);
+        r[i] = sqrt(pow(x[i],2) + pow(y[i],2));
+        
+        //leap-frog
+        if(i==1)
+        {
+            x_[i] = x[i];
+            y_[i] = y[i];
+            vx_[i] = vx[i];
+            vy_[i] = vy[i];
+        }
+        
+        else
+        {
+            vx_[i] = vx_[i-2] + 2*dt*aceleracion(x_[i-1], r_[i-1]);
+            vy_[i] = vy_[i-2] + 2*dt*aceleracion(y_[i-1], r_[i-1]);
+            x_[i] = leap_frog(x_[i-2], vx_[i-1]);
+            y_[i] = leap_frog(y_[i-2], vy_[i-1]);
+        }
+        
+        r_[i] = sqrt(pow(x_[i],2) + pow(y_[i],2));
+        
+        
+        archivo << x[i] << " " << y[i] << endl;
+        archivo1 << x_[i] << " " << y_[i] << endl;
     }
     
+    archivo.close();
+    archivo1.close();
     
+    //runge kutta
+    runge_kutta();
     
     return 0;    
 }
@@ -85,39 +111,70 @@ double euler(double pos_antes, double v_antes)
     return pos_antes + dt*v_antes;
 }
 
-double leap_frog(double pos_antes_antes, double v_actual)
+double leap_frog(double pos_antes_antes, double v_antes)
 {
-    return pos_antes_antes + 2*dt*v_actual;
+    return pos_antes_antes + 2*dt*v_antes;
 }
 
-double runge_kutta(double pos_antes, double v_antes, double r_antes)
+double runge_kutta()
 {
-    double k1_1 = pos_antes;
-    double k1_2 = aceleracion(pos_antes, r_antes);
+    ofstream archivo2;
+    archivo2.open("runge_kutta.dat");
     
-    double pos1 = pos_antes + (dt/2) * k1_1;
-    double v1 = v_antes + (dt/2) * k1_2;
+    archivo2 << _x_[0] << " " << _y_[0] << endl;
     
-    double k2_1 = v1;
-    double k2_2 = aceleracion(pos1, r_antes);
+    for(int i=1; i<n; i++)
+    {
+        double k1_1_x = _x_[i-1];
+        double k1_2_x = aceleracion(_x_[i-1], _r_[i-1]);
+        double k1_1_y = _y_[i-1];
+        double k1_2_y = aceleracion(_y_[i-1], _r_[i-1]);
     
-    double pos2 = pos_antes + (dt/2) * k2_1;
-    double v2 = v_antes + (dt/2) * k2_2;
+        double pos1_x = _x_[i-1] + (dt/2) * k1_1_x;
+        double v1_x = _vx_[i-1] + (dt/2) * k1_2_x;
+        double pos1_y = _y_[i-1] + (dt/2) * k1_1_y;
+        double v1_y = _vy_[i-1] + (dt/2) * k1_2_y;
+    
+        double k2_1_x = v1_x;
+        double k2_2_x = aceleracion(pos1_x, _r_[i-1]);
+        double k2_1_y = v1_y;
+        double k2_2_y = aceleracion(pos1_y, _r_[i-1]);
+    
+        double pos2_x = _x_[i-1] + (dt/2) * k2_1_x;
+        double v2_x = _vx_[i-1] + (dt/2) * k2_2_x;
+        double pos2_y = _y_[i-1] + (dt/2) * k2_1_y;
+        double v2_y = _vy_[i-1] + (dt/2) * k2_2_y;
 
-    double k3_1 = v2;
-    double k3_2 = aceleracion(pos2, r_antes);
+        double k3_1_x = v2_x;
+        double k3_2_x = aceleracion(pos2_x, _r_[i-1]);
+        double k3_1_y = v2_y;
+        double k3_2_y = aceleracion(pos2_y, _r_[i-1]);
 
-    double pos3 = pos_antes + dt * k3_1;
-    double v3  = v_antes + dt * k3_2;
+        double pos3_x = _x_[i-1] + dt * k3_1_x;
+        double v3_x  = _vx_[i-1] + dt * k3_2_x;
+        double pos3_y = _y_[i-1] + dt * k3_1_y;
+        double v3_y  = _vy_[i-1] + dt * k3_2_y;
 
-    double k4_1 = v3;
-    double k4_2 = aceleracion(pos3, r_antes);
+        double k4_1_x = v3_x;
+        double k4_2_x = aceleracion(pos3_x, _r_[i-1]);
+        double k4_1_y = v3_y;
+        double k4_2_y = aceleracion(pos3_y, _r_[i-1]);
  
-    double k1_promedio = (k1_der_1 + 2*k2_der_1 + 2*k3_der_1 + k4_der_1)/6 ;
-    double k2_promedio = (k1_der_2 + 2*k2_der_2 + 2*k3_der_2 + k4_der_2)/6 ;  
+        double k1_promedio_x = (k1_1_x + 2*k2_1_x + 2*k3_1_x + k4_1_x)/6 ;
+        double k2_promedio_x = (k1_2_x + 2*k2_2_x + 2*k3_2_x + k4_2_x)/6 ; 
+        double k1_promedio_y = (k1_1_y + 2*k2_1_y + 2*k3_1_y + k4_1_y)/6 ;
+        double k2_promedio_y = (k1_2_y + 2*k2_2_y + 2*k3_2_y + k4_2_y)/6 ; 
 
-    double pos_ahora = pos_antes + dt * k1_promedio;
-    double v_ahora = v_antes + dt * k2_promedio;
+        _vx_[i] = _vx_[i-1] + dt*k2_promedio_x;
+        _x_[i] = _x_[i-1] + dt*k1_promedio_x;
+        _vy_[i] = _vy_[i-1] + dt*k2_promedio_y;
+        _y_[i] = _y_[i-1] + dt*k1_promedio_y;
+        
+        _r_[i] = sqrt(pow(_x_[i],2) + pow(_y_[i],2));
+        
+        archivo2 << _x_[i] << " " << _y_[i] << endl;
+        
+    }
     
-    return pos_ahora;
+    archivo2.close();
 } 
