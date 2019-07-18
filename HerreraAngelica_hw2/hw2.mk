35193726f5ebaac0356f16d0fc1f07375d311c4d
Default user <angelicaherreraalba@gmail.com>
2019-07-18 01:37:07 +0000

    .mk para hacer pdflatex

--- HerreraAngelica_hw2/hw2.mk
+++ HerreraAngelica_hw2/hw2.mk
@@ -1,4 +1,7 @@
-all : Plots_hw2.py
+Resultados_hw2.tex : y_vs_x.png vy_vs_vx.png L_vs_t.png E_vs_t.png
+	pdflatex Resultados_hw2.tex
+
+y_vs_x.png vy_vs_vx.png L_vs_t.png E_vs_t.png : Plots_hw2.py
 	python Plots_hw2.py
 
 Plots_hw2.py : euler_dt1.dat leap_frog_dt1.dat runge_kutta_dt1.dat euler_dt2.dat leap_frog_dt2.dat runge_kutta_dt2.dat euler_dt3.dat leap_frog_dt3.dat runge_kutta_dt3.dat
