52a30d6d3f3761c786efc7a1bdefdcfcdcded6b4
Default user <angelicaherreraalba@gmail.com>
2019-07-19 00:50:52 +0000

    avance fourier.py

--- HerreraAngelica_hw2/Fourier.py
+++ HerreraAngelica_hw2/Fourier.py
@@ -2,77 +2,73 @@ import numpy as np
 import matplotlib.pylab as plt
 from scipy.fftpack import fft2, ifft2, fftshift, ifftshift
 
-#Almaceno los datos las dos imágenes
-im1 = plt.imread("cara_03_grisesMF.png")   #imagen sorprendida
-im2 = plt.imread("cara_02_grisesMF.png")   #imagen feliz
+# Almaceno los datos las dos imágenes
+im1 = plt.imread("cara_03_grisesMF.png")      #imagen seria
+im2 = plt.imread("cara_02_grisesMF.png")      #imagen feliz
 
-#Transformada de Fourier en 2D de ambas imágenes
+# Transformada de Fourier en 2D de ambas imágenes
 fourier_im1 = fft2(im1)
 fourier_im2 = fft2(im2)
 
-#Gráfica del espectro de Fourier de cada imagen
+# Gráfica del espectro de Fourier de cada imagen
 from matplotlib.colors import LogNorm
-plt.figure()
+
+plt.figure(figsize=(10,10))
+plt.subplot(1,2,1)
 plt.imshow(abs(fourier_im1), norm=LogNorm())
 plt.title("Espectro de Fourier de im1")
-plt.savefig("FFT2D_im1.png")
-
-plt.figure()
+plt.subplot(1,2,2)
 plt.imshow(abs(fourier_im2), norm=LogNorm())
 plt.title("Espectro de Fourier de im2")
-plt.savefig("FFT2D_im2.png")
+plt.savefig("FFtIm.pdf")
 
-#Grafica de las frecuencias de cada imagen
+# Frecuencias de cada imagen
 freq_im1 = fftshift(fourier_im1)
 freq_im2 = fftshift(fourier_im2)
 
-plt.figure()
+# Filtros
+# Filtro paso alto para imagen de cerca
+fc_1 = 40
+freq_im1_ = np.copy(freq_im1)
+ind_im1 = np.where(abs(freq_im1_) < fc_1)   #indices de mis frecuencias malas
+freq_im1_[ind_im1] = 0   #mando a 0 las frecuencias malas
+
+# Filtro paso bajo para imagen de lejos
+fc_2 = 30
+freq_im2_ = np.copy(freq_im2)
+ind_im2 = np.where(abs(freq_im2_) > fc_2)
+freq_im2_[ind_im2] = 0
+
+plt.figure(figsize=(10,10))
+plt.subplot(2,2,1)
 plt.imshow(abs(freq_im1), norm=LogNorm())
 plt.colorbar()
 plt.title("Espectro de frecuencias de im1")
-plt.savefig("freq_im1.png")
-
-plt.figure()
+plt.subplot(2,2,2)
 plt.imshow(abs(freq_im2), norm=LogNorm())
 plt.colorbar()
 plt.title("Espectro de frecuencias de im2")
-plt.savefig("freq_im2.png")
-
-#Filtros y sus graficas
-#filtro paso alto para imagen de cerca
-fc_1 = 40
-ind_im1 = np.where(abs(freq_im1) < fc_1)   #indices de mis frecuencias malas
-freq_im1[ind_im1] = 0   #mando a 0 las frecuencias malas
-
-plt.figure()
-plt.imshow(abs(freq_im1), norm=LogNorm())
+plt.subplot(2,2,3)
+plt.imshow(abs(freq_im1_), norm=LogNorm())
 plt.colorbar()
 plt.title("Espectro de filtrado im1")
-plt.savefig("filtro_im1.png")
-
-#filtro paso bajo para imagen de lejos
-fc_2 = 30
-ind_im2 = np.where(abs(freq_im2) > fc_2)
-freq_im2[ind_im2] = 0
-
-plt.figure()
-plt.imshow(abs(freq_im2), norm=LogNorm())
+plt.subplot(2,2,4)
+plt.imshow(abs(freq_im2_), norm=LogNorm())
 plt.colorbar()
 plt.title("Espectro de filtrado im2")
-plt.savefig("filtro_im2.png")
+plt.savefig("ImProceso.pdf")
 
-#Saco espectros de Fourier correspondientes a las frecuencias filtradas
-trans_im1 = ifftshift(freq_im1) 
-trans_im2 = ifftshift(freq_im2)
+# Saco espectros de Fourier correspondientes a las frecuencias filtradas
+trans_im1 = ifftshift(freq_im1_) 
+trans_im2 = ifftshift(freq_im2_)
 
-#Suma de espectros correspondientes a frecuencias filtradas
+# Suma de espectros correspondientes a frecuencias filtradas
 trans = trans_im1*0.3 + trans_im2
 
-#Grafica de ambas juntas (imagen hibrida)
+# Grafica de ambas juntas (imagen hibrida)
 im_hibrida = ifft2(trans)
 
 plt.figure()
 plt.imshow(abs(im_hibrida), plt.cm.gray)
 plt.title('Imagen hibrida')
-plt.savefig("im_hibrida.png")
-
+plt.savefig("ImHybrid.pdf")
